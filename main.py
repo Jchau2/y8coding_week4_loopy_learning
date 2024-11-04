3124a7f2f1733fddd6bb8071c87452226607203b
Jchau2 <jchau@kellettschool.com>
2024-11-04 06:05:09 +0000

    Final

--- main.py
+++ main.py
@@ -4,54 +4,113 @@
 
 print("\nTASK 1 HERE\n")
 
+	## TASK 1
+for i in range(5):	
+    print("Bob")
 
 
 print("\nTASK 2 HERE\n")
-
+## TASK 2
+for j in range(7):	
+    print("🍜 Noodles")
+    print("🍚 Rice")         # this is OUTSIDE of the loop.
 
 
 print("\nTASK 3 HERE\n")
-
+## TASK 3
+for j in range(3):	
+    print("🦖 Dinosaur")
+    print("🐘 Elephant")
 
 
 print("\nTASK 4 HERE\n")
-
+## TASK 4
+for x in range(6):	
+    print("🐶 Dog")
+print("🐱 Cat")
 
 
 print("\nTASK 5 HERE\n")
 
+print("🐁 Mouse")
 
-
+for x in range (4):
+    print("🐸 Frog")
+print("🐎 Horse")
 print("\nTASK 6 HERE\n")
 
-
+for x in range(2):	
+    print("🐶 Dog")
+    print("🐱 Cat")
 
 print("\nTASK 7 HERE\n")
 
-
+for x in range(3):	
+    print("🦒 Giraffe")
+    print("🐒 Monkey")
 
 print("\nTASK 8 HERE\n")
 
-
+print("🐯 Tiger")
+print("🦁 Lion")
+print("🐯 Tiger")
 
 print("\nTASK 9 HERE\n")
 
-
+print("🙈 Chimp")              ## before BOTH loops begin
+for x in range(3):	
+    for y in range(3):	
+        print("🦔 Hedgehog")   ## inside the inner loop
+    print("🐮 Cow")            ## outside of the inner loop, but in the outer loop!
+print("🦍 Gorilla")            ## after BOTH loops have ended
 
 print("\nTASK 10 HERE\n")
 
-
+for i in range(2):	
+    for j in range(4):	
+        print("🐍 Snake")
+        print("🦎 Lizard")
 
 print("\nTASK 11 HERE\n")
 
-
+print("🐹 Hamster")
+for i in range(2):	
+    for j in range(2):	
+        print("🐇 Bunny")
+        print("🐇 Bunny")
+        print("🐰 Rabbit")
+        
 
 print("\nTASK 12 HERE\n")
 
-
+for j in range(2):	
+    print("🐧 Penguin")
+    print("🐔 Chicken")
+    print("🐔 Chicken")
+    print("🐔 Chicken")
+    print("🦉 Owl")
 
 print("\nTASK 13 HERE\n")
 
 
+for i in range(2):	
+    for j in range(3):	
+            print("🐶 Dog")
+    print("🐶 Dog", "🐱 Cat")
+    print("🐱 Cat")
 
+    for d in range(2):
+        print("🐱 Cat 🐱 Cat")
+        
 print("\nTASK 14 HERE\n")
+print("🐐 Goat")
+for l in range(2):
+    print("🐑 Sheep")
+    print("🐑 Sheep")
+    for w in range(4):
+        print("🐐 Goat")
+
+for _ in range(2):
+    print("🐑 Sheep")
+for p in range(3):
+    print("🐐 Goat")
